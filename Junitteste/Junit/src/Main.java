f7411c079f09a41172497c2c6e6ff2b0912b0899
Marco Antonio <marcotonho38@gmail.com>
2023-04-14 19:01:31 -0300

    Adicionando case 6 historico

--- Junitteste/Junit/src/Main.java
+++ Junitteste/Junit/src/Main.java
@@ -1,5 +1,5 @@
 import Calculo.Calculadora;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +8,7 @@ public class Main {
         int opcao;
         boolean continuar = true;
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> historico = new ArrayList<>();
 
         do {
             System.out.println("======CALCULADORA======");
@@ -17,48 +18,88 @@ public class Main {
             System.out.println("3: MULTIPLICAR");
             System.out.println("4: DIVIDIR");
             System.out.println("5: POTENCIAR");
-            System.out.println("6: SAIR");
+            System.out.println("6: HISTORICO");
+            System.out.println("7: SAIR");
             System.out.println("=======================");
             opcao = sc.nextInt();
-            System.out.print("Escolha o 1º número que deseja calcular : ");
-            n1 = sc.nextDouble();
-            System.out.print("Escolha o 2º número que deseja calcular : ");
-            n2 = sc.nextDouble();
-            Calculadora calculadora = new Calculadora(n1, n2);
-
             switch (opcao) {
                 case 1:
                     System.out.println("======SOMA======");
-                    System.out.println("O resultado da soma é: " + calculadora.soma());
+                    System.out.print("Escolha o 1º número que deseja calcular : ");
+                    n1 = sc.nextDouble();
+                    System.out.print("Escolha o 2º número que deseja calcular : ");
+                    n2 = sc.nextDouble();
+                    Calculadora calculadora1 = new Calculadora(n1, n2);
+                    System.out.println("O resultado da soma é: " + calculadora1.soma());
+                    historico.add(n1 + " + " + n2 + " = " + calculadora1.soma());
                     break;
                 case 2:
                     System.out.println("======SUBTRAÇÃO======");
-                    System.out.println("O resultado da subtração é: " + calculadora.subtracao());
+                    System.out.print("Escolha o 1º número que deseja calcular : ");
+                    n1 = sc.nextDouble();
+                    System.out.print("Escolha o 2º número que deseja calcular : ");
+                    n2 = sc.nextDouble();
+                    Calculadora calculadora2 = new Calculadora(n1, n2);
+                    System.out.println("O resultado da subtração é: " + calculadora2.subtracao());
+                    historico.add(n1 + " - " + n2 + " = " + calculadora2.subtracao());
+
                     break;
                 case 3:
                     System.out.println("======MULTIPLICAR======");
-                    System.out.println("O resultado da multiplicação é: " + calculadora.multiplicacao());
+                    System.out.print("Escolha o 1º número que deseja calcular : ");
+                    n1 = sc.nextDouble();
+                    System.out.print("Escolha o 2º número que deseja calcular : ");
+                    n2 = sc.nextDouble();
+                    Calculadora calculadora3 = new Calculadora(n1, n2);
+                    System.out.println("O resultado da multiplicação é: " + calculadora3.multiplicacao());
+                    historico.add(n1 + " * " + n2 + " = " + calculadora3.multiplicacao());
+
                     break;
                 case 4:
                     System.out.println("======DIVISÃO======");
-                    double resultadoDivisao = calculadora.divisao();
+                    System.out.print("Escolha o 1º número que deseja calcular : ");
+                    n1 = sc.nextDouble();
+                    System.out.print("Escolha o 2º número que deseja calcular : ");
+                    n2 = sc.nextDouble();
+                    Calculadora calculadora4 = new Calculadora(n1, n2);
+                    historico.add(n1 + " /" + n2 + " = " + calculadora4.divisao());
+
+                    double resultadoDivisao = calculadora4.divisao();
                     if (resultadoDivisao != 0) {
                         System.out.println("O resultado da divisão é: " + resultadoDivisao);
                     }
                     break;
                 case 5:
                     System.out.println("======POTENCIAR======");
-                    System.out.println("O resultado da potenciação é: " + calculadora.potenciacao());
+                    System.out.print("Escolha o 1º número que deseja calcular : ");
+                    n1 = sc.nextDouble();
+                    System.out.print("Escolha o 2º número que deseja calcular : ");
+                    n2 = sc.nextDouble();
+                    Calculadora calculadora5 = new Calculadora(n1, n2);
+                    System.out.println("O resultado da potenciação é: " + calculadora5.potenciacao());
+                    historico.add(n1 + " ^" + n2 + " = " + calculadora5.potenciacao());
+
+                case 6:
+                    System.out.println("======HISTÓRICO======");
+                    if (historico.isEmpty()) {
+                        System.out.println("Não há operações registradas no histórico ainda.");
+                    } else {
+                        System.out.println("Operações registradas no histórico:");
+                        for (String item : historico) {
+                            System.out.println(item);
+                        }
+                    }
                     break;
+                case 7:
+                    System.out.println("======SAIR======");
+                    continuar = false;
+                    System.out.println("Fim do programa");
+                    break;
+
                 default:
                     System.out.println("Opção inválida!");
                     break;
-            }
-            System.out.println("Deseja fazer outra operação? 1 - sim, 2 - não");
-            int escolha = sc.nextInt();
-            if (escolha == 2) {
-                continuar = false;
-                System.out.println("Fim do programa");
+
             }
 
         } while (continuar);
@@ -66,4 +107,5 @@ public class Main {
         sc.close();
 
     }
+
 }
